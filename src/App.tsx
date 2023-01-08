c75c8c7f084cc09c0b776efcfa4c8f8001c41a73
Emmanuel Raji <ertech171@gmail.com>
2023-01-08 17:11:32 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
