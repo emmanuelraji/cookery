d58ee8d938e4569072fb381b7cb843587d274465
Emmanuel Raji <ertech171@gmail.com>
2023-01-17 08:32:04 -0500

    configure active states

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,33 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { categories } from "../lib/categories";
 import cusine from "../assets/all-cuisines.png";
 
+type foodType = {
+  name: string;
+  selected: boolean;
+};
+
 function Category() {
-  const [foodItems, setFoodItems] = useState(categories.essentials);
+  const [foodItems, setFoodItems] = useState<foodType[]>([]);
   const [numOfItemsSelected, setNumOfItemsSelectedCount] = useState(0);
+  const [limit, setLimit] = useState(10);
+  // const [isActive, setIsActive] = useState(false);
+  const numOfCategoryItem = categories.essentials.length;
+
+  useEffect(() => {
+    const foods = categories.essentials.filter((_, index) => index < limit);
+    setFoodItems(foods);
+  }, [limit]);
 
   function handleChange(name: string) {
     const foods = foodItems.map((food) => {
-      // return food.name === name ? { ...food, selected: !food.selected } : food;
-      if (food.name === name) {
-        return { ...food, selected: !food.selected };
-      }
-      return food;
+      return food.name === name ? { ...food, selected: !food.selected } : food;
     });
+
     const count = foods.filter((item) => item.selected === true).length;
+
     setFoodItems(foods);
     setNumOfItemsSelectedCount(count);
   }
 
   return (
     <div className="bg-white shadow-md rounded">
-      <div className="flex mb-1 p-6">
+      <div className="flex mb-1 p-6 justify-between">
         <img src={cusine} alt="all-cusines" className="w-12 h-12" />
         <div>
           <h2>Pantry Essentials</h2>
           <p className="font-light text-gray-500">
-            {numOfItemsSelected}/{foodItems.length} Ingredients
+            {numOfItemsSelected}/{numOfCategoryItem} Ingredients
           </p>
+        </div>
+        <div>
+          {limit === 10 ? (
+            <span onClick={() => setLimit(numOfCategoryItem)}>+</span>
+          ) : (
+            <span onClick={() => setLimit(10)}>-</span>
+          )}
         </div>
       </div>
       <div className="px-6 pb-6">
@@ -46,6 +64,14 @@ function Category() {
             </span>
           );
         })}
+        {limit === 10 && (
+          <span
+            onClick={() => setLimit(numOfCategoryItem)}
+            className="text-gray-500 font-light p-2 bg-neutral-100 rounded inline-block mr-2 mt-2"
+          >
+            +{numOfCategoryItem - foodItems.length} More
+          </span>
+        )}
       </div>
     </div>
   );
